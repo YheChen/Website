1a159ed354f5c3c750873ef73808c8e1cc202dfc
YheChen <yanzhenchen123@gmail.com>
2024-03-13 18:36:37 -0400

    changed some pictures

--- src/pages/sidescroll.jsx
+++ src/pages/sidescroll.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
 import "./sidescroll.css";
 import { Link } from "react-router-dom";
+import rooftop from "./images/rooftop.jpg";
 import img7 from "./images/7.jpg";
-import img11 from "./images/11.jpg";
+import img16 from "./images/16.jpg";
 import img8 from "./images/8.jpg";
 import img2 from "./images/2.jpg";
 import img12 from "./images/12.jpg";
 import img9replacement from "./images/9replacement.png";
 import img10 from "./images/10.jpg";
-import img4 from "./images/4.jpg";
-import img3replacement from "./images/3replacement.png";
+import img14 from "./images/14.jpg";
+import img17 from "./images/17.jpg";
 import lastdayofsummer from "./images/lastdayofsummer.png";
 
 function Sidescroll() {
@@ -121,15 +122,16 @@ function Sidescroll() {
         </Link>
       </div>
       <div id="image-track" data-mouse-down-at="0" data-prev-percentage="0">
+        <img className="image" alt="" src={rooftop} draggable="false" />
         <img className="image" alt="" src={img7} draggable="false" />
-        <img className="image" alt="" src={img11} draggable="false" />
+        <img className="image" alt="" src={img16} draggable="false" />
         <img className="image" alt="" src={img8} draggable="false" />
         <img className="image" alt="" src={img2} draggable="false" />
         <img className="image" alt="" src={img12} draggable="false" />
         <img className="image" alt="" src={img9replacement} draggable="false" />
         <img className="image" alt="" src={img10} draggable="false" />
-        <img className="image" alt="" src={img4} draggable="false" />
-        <img className="image" alt="" src={img3replacement} draggable="false" />
+        <img className="image" alt="" src={img14} draggable="false" />
+        <img className="image" alt="" src={img17} draggable="false" />
         <img className="image" alt="" src={lastdayofsummer} draggable="false" />
       </div>
     </div>
